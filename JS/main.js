60bd38f660b2f89d18ebf50af1d8be359e5af5ca
mudit8928 <muditsingh8928@gmail.com>
2024-10-15 23:36:49 +0530

    Enhancement in the code-2

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,26 @@
-gsap.to ("#line-svg", {
-    duration: 1,
-    x: 12,
-    y:14,
-    delay: 1
-});
-let svg = document.querySelector("#line-svg");
+var path = `M15 75 Q 581.5 75 1165 75`;
+var finalpath = 'M15 75 Q 581.5 75 1165 75';
+let svg = document.querySelector("#string");
 svg.addEventListener("mousemove",(dets) => {
-    console.log(dets.x,dets.y);
+    path = `M15 75 Q ${dets.x} ${dets.y-80} 1165 75`;
+    gsap.to("svg path",{
+        attr: {d : path},
+        duration: .2,
+        ease: "power3.out",
+    })
+})
+svg.addEventListener("mouseleave",() => {
+    gsap.to("svg path",{
+        attr: {d : finalpath},
+        duration: .3,
+        ease: "elastic.out(1.5,0.5)",
+    });
+})
+let left = document.querySelector("#left-btn")
+left.addEventListener("click",() => {
+    console.log("left-clicked")
+});
+let right = document.querySelector("#right-btn")
+right.addEventListener("click",() => {
+    console.log("right-clicked");
 })
